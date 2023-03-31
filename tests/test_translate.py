0c9148a7d0a29bce198013e174d6dab3243c9284
gram <git@orsinium.dev>
2023-03-31 11:13:00 +0200

    test translator

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+import pydantic
+import pytest
+
+from pydantic_translations import Translator
 from pydantic_translations._translator import _format
 
 
@@ -10,3 +14,23 @@ def test_format() -> None:
         eng_message='oh hi mark',
     )
     assert actual == 'о, привет, mark'
+
+
+@pytest.mark.parametrize('lang, given, expected', [
+    ('ru', {'name': 'Aragorn', 'age': 88}, None),
+    ('ru', {'name': '', 'age': 'hi'}, 'значение должно быть целым числом'),
+    ('en', {'name': '', 'age': 'hi'}, 'value is not a valid integer'),
+])
+def test_translator(lang: str, given: object, expected: str | None) -> None:
+    class User(pydantic.BaseModel):
+        name: str
+        age: int = 21
+
+    tr = Translator(lang)
+    try:
+        User.parse_obj(given)
+    except pydantic.ValidationError as exc:
+        actual = tr.translate_exception(exc)
+        assert actual.errors()[0]['msg'] == expected
+    else:
+        assert expected is None
